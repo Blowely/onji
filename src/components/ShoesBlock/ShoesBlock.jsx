0b071d3c55b6d0cdf0ea7544ce096dabf76fa99e
maryashin.andrey <maryashin.andrey@wb.ru>
2025-04-28 21:26:15 +0300

    ShoesBlock changed content

--- src/components/ShoesBlock/ShoesBlock.jsx
+++ src/components/ShoesBlock/ShoesBlock.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from './ShoesBlock.module.scss';
 import s1 from "../../assets/shoesFeatures/s1.png";
+import s2 from "../../assets/shoesFeatures/s2.png";
+import s3 from "../../assets/shoesFeatures/s3.png";
+import p1 from "../../assets/newFeatures/p1.png";
 import p2 from "../../assets/newFeatures/p2.png";
 import p5 from "../../assets/newFeatures/p5.png";
 import p6 from "../../assets/newFeatures/p6.png";
@@ -15,22 +18,46 @@ const products = [
         image: s1,
     },
     {
-        name: 'NEW BALANCE 9060',
-        price: 'от 5999 ₽',
+        name: 'adidas originals SAMBA OG',
+        price: 'от 15589 ₽',
         spuId: 4239122,
-        image: p2,
+        image: s2,
     },
     {
-        name: 'ADIDAS INTIMIDATION LOW',
+        name: 'New Balance NB 2002R',
         price: 'от 5999 ₽',
         spuId: 4239122,
-        image: p5,
+        image: s3,
     },
     {
         name: 'ASICS GEL-NYC',
         price: 'от 5999 ₽',
         spuId: 4239122,
         image: p6,
+    },
+    {
+        image: p1,
+        spuId: 5321945,
+        name: 'NIKE P-6000',
+        category: 'КРОССОВКИ',
+        price: 'от 5999 ₽',
+        discountedPrice: ''
+    },
+    {
+        image: p2,
+        spuId: 4239122,
+        name: 'NEW BALANCE 9060',
+        category: 'КРОССОВКИ',
+        price: 'от 5999 ₽',
+        discountedPrice: ''
+    },
+    {
+        image: p5,
+        spuId: 21720097,
+        name: 'ADIDAS INTIMIDATION LOW',
+        category: 'КРОССОВКИ',
+        price: 'от 11609 ₽',
+        discountedPrice: ''
     },
 ];
 
@@ -70,29 +97,53 @@ const ShoesBlock = () => {
                             </div>
                         </div>
                         <div
-                            className={styles.productCard}
-                            onClick={() => navigate(`?spuId=${products[3].spuId}`)}
                         >
-                            <img alt={products[2].name} src={products[2].image} className={styles.productImage}/>
-                            <div className={styles.productInfo}>
-                                <div className={styles.productName}>{products[2].name}</div>
-                                <div className={styles.productPrice}>{products[2].price}</div>
-                            </div>
                         </div>
                     </div>
 
                 </div>
                 <div className={styles.columnsWrapper}>
-                    <div
-                        className={styles.productCard}
-                        style={{width: '100%', height: '100%'}}
-                        onClick={() => navigate(`?spuId=${products[3].spuId}`)}
-                    >
-                        <img alt={products[3].name} src={products[3].image} className={styles.productImage}
-                             style={{width: '100%', height: 'calc(100% - 70px)'}}/>
-                        <div className={styles.productInfo}>
-                            <div className={styles.productName}>{products[3].name}</div>
-                            <div className={styles.productPrice}>{products[3].price}</div>
+                    <div className={styles.columnContainer}>
+                        <div
+                            style={{width: '100%', height: '100%'}}
+                        />
+                        <div
+                            className={styles.productCard}
+                            style={{width: '100%', height: '100%'}}
+                            onClick={() => navigate(`?spuId=${products[4].spuId}`)}
+                        >
+                            <img alt={products[4].name} src={products[4].image} className={styles.productImage}
+                                 style={{width: '100%', height: 'calc(100% - 70px)'}}/>
+                            <div className={styles.productInfo}>
+                                <div className={styles.productName}>{products[4].name}</div>
+                                <div className={styles.productPrice}>{products[4].price}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.columnContainer}>
+                        <div
+                            className={styles.productCard}
+                            style={{width: '100%', height: '100%'}}
+                            onClick={() => navigate(`?spuId=${products[5].spuId}`)}
+                        >
+                            <img alt={products[5].name} src={products[5].image} className={styles.productImage}
+                                 style={{width: '100%', height: 'calc(100% - 70px)'}}/>
+                            <div className={styles.productInfo}>
+                                <div className={styles.productName}>{products[5].name}</div>
+                                <div className={styles.productPrice}>{products[5].price}</div>
+                            </div>
+                        </div>
+                        <div
+                            className={styles.productCard}
+                            style={{width: '100%', height: '100%'}}
+                            onClick={() => navigate(`?spuId=${products[6].spuId}`)}
+                        >
+                            <img alt={products[6].name} src={products[6].image} className={styles.productImage}
+                                 style={{width: '100%', height: 'calc(100% - 70px)'}}/>
+                            <div className={styles.productInfo}>
+                                <div className={styles.productName}>{products[6].name}</div>
+                                <div className={styles.productPrice}>{products[6].price}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
