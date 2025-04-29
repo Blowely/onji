74451c66e17929b03b5cdc6990c89d5f53442e86
maryashin.andrey <maryashin.andrey@wb.ru>
2025-04-29 21:37:08 +0300

    added req product block

--- src/components/ReqProduct/ReqProducts.jsx
+++ src/components/ReqProduct/ReqProducts.jsx
@@ -1,9 +1,15 @@
 import styles from './ReqProducts.module.scss';
 
 const ReqProducts = () => {
-    return (<div className={styles.container}>
-        <img src="https://storage.yandexcloud.net/pc-mediafiles/test1/banners/banner3.webp" alt=""/>
-    </div>)
+    return <div className={styles.container}>
+        <img src={"https://storage.yandexcloud.net/pc-mediafiles/test1/banners/banner3.webp"} alt="Background" className={styles.image} />
+        <div className={styles.textOverlay}>
+            <p>НЕ НАШЛИ<br /><div style={{textAlign: 'start'}}>
+                НУЖНЫЙ ТОВАР?<br />ПРИВЕЗЁМ ПОД <br/> <span style={{float: 'left'}}>ЗАКАЗ</span>
+            </div></p>
+            <a href="#" className={styles.link}>ЗАКАЗАТЬ</a>
+        </div>
+    </div>
 }
 
 export default ReqProducts;
