0d05d0cd9fc074b64c3afb6d96193ef4ce1b082a
maryashin.andrey <maryashin.andrey@wb.ru>
2025-04-30 14:03:25 +0300

    added req product block

--- src/components/ReqProduct/ReqProducts.jsx
+++ src/components/ReqProduct/ReqProducts.jsx
@@ -5,7 +5,9 @@ const ReqProducts = () => {
         <img src={"https://storage.yandexcloud.net/pc-mediafiles/test1/banners/banner3.webp"} alt="Background" className={styles.image} />
         <div className={styles.textOverlay}>
             <p>НЕ НАШЛИ<br /><div style={{textAlign: 'start'}}>
-                НУЖНЫЙ ТОВАР?<br />ПРИВЕЗЁМ ПОД <br/> <span style={{float: 'left'}}>ЗАКАЗ</span>
+                <span>
+                    НУЖНЫЙ ТОВАР?
+                </span><br />ПРИВЕЗЁМ ПОД <br/> <span>ЗАКАЗ</span>
             </div></p>
             <a href="#" className={styles.link}>ЗАКАЗАТЬ</a>
         </div>
