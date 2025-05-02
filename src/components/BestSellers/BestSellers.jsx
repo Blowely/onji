f4045a0d88795d33f82ed4af0af74f75739231cc
maryashin.andrey <maryashin.andrey@wb.ru>
2025-05-02 14:22:34 +0300

    BestSellers.jsx refactoring to phone

--- src/components/BestSellers/BestSellers.jsx
+++ src/components/BestSellers/BestSellers.jsx
@@ -12,30 +12,35 @@ const products = [
     {
         name: 'NIKE ZOOM',
         price: 'от 5999 ₽',
+        category: 'КРОССОВКИ',
         spuId: 4239122,
         image: p1,
     },
     {
         name: 'NEW BALANCE 9060',
         price: 'от 5999 ₽',
+        category: 'КРОССОВКИ',
         spuId: 4239122,
         image: p2,
     },
     {
         name: 'ADIDAS FW24 BASIC DOWN',
         price: 'от 5999 ₽',
+        category: 'КРОССОВКИ',
         spuId: 4239122,
         image: p3,
     },
     {
         name: 'MICHAEL MICHAEL KORS',
         price: 'от 5999 ₽',
+        category: 'КРОССОВКИ',
         spuId: 4239122,
         image: p4,
     },
     {
         name: 'ASICS',
         price: 'от 5999 ₽',
+        category: 'КРОССОВКИ',
         spuId: 4239122,
         image: p6,
     },
@@ -47,7 +52,7 @@ const BestSellers = () => {
     const isDesktop = window?.innerWidth > 768;
 
     return (
-        <div style={{marginTop:  isDesktop ? '100px' : 'unset'}}>
+        <div className={styles.mainContainer}>
             {isDesktop &&
                 <div className={styles.showAll}>
                     смотреть все
@@ -58,6 +63,7 @@ const BestSellers = () => {
                 <div className={styles.container}>
                     <div className={styles.columnsWrapper}>
                         <div className={styles.columnContainer}>
+                            <RotatingTextCircle/>
                             <div
                                 className={styles.productCard}
                                 onClick={() => navigate(`?spuId=${products[3].spuId}`)}
@@ -65,16 +71,7 @@ const BestSellers = () => {
                                 <img alt={products[0].name} src={products[0].image} className={styles.productImage}/>
                                 <div className={styles.productInfo}>
                                     <div className={styles.productName}>{products[0].name}</div>
-                                    <div className={styles.productPrice}>{products[0].price}</div>
-                                </div>
-                            </div>
-                            <div
-                                className={styles.productCard}
-                                onClick={() => navigate(`?spuId=${products[3].spuId}`)}
-                            >
-                                <img alt={products[0].name} src={products[0].image} className={styles.productImage}/>
-                                <div className={styles.productInfo}>
-                                    <div className={styles.productName}>{products[0].name}</div>
+                                    <div className={styles.categoryName}>{products[0].category}</div>
                                     <div className={styles.productPrice}>{products[0].price}</div>
                                 </div>
                             </div>
@@ -85,8 +82,9 @@ const BestSellers = () => {
                                 onClick={() => navigate(`?spuId=${products[3].spuId}`)}
                             >
                                 <img alt={products[1].name} src={products[1].image} className={styles.productImage}/>
-                                <div className={styles.productInfo}>
+                                <div className={styles.productInfo} style={{paddingLeft: '20px'}}>
                                     <div className={styles.productName}>{products[1].name}</div>
+                                    <div className={styles.categoryName}>{products[1].category}</div>
                                     <div className={styles.productPrice}>{products[1].price}</div>
                                 </div>
                             </div>
@@ -97,6 +95,7 @@ const BestSellers = () => {
                                 <img alt={products[4].name} src={products[4].image} className={styles.productImage}/>
                                 <div className={styles.productInfo}>
                                     <div className={styles.productName}>{products[4].name}</div>
+                                    <div className={styles.categoryName}>{products[4].category}</div>
                                     <div className={styles.productPrice}>{products[4].price}</div>
                                 </div>
                             </div>
@@ -117,6 +116,7 @@ const BestSellers = () => {
                                 <img alt={products[0].name} src={products[0].image} className={styles.productImage}/>
                                 <div className={styles.productInfo}>
                                     <div className={styles.productName}>{products[0].name}</div>
+                                    <div className={styles.categoryName}>{products[0].category}</div>
                                     <div className={styles.productPrice}>{products[0].price}</div>
                                 </div>
                             </div>
@@ -129,6 +129,7 @@ const BestSellers = () => {
                                 <img alt={products[1].name} src={products[1].image} className={styles.productImage}/>
                                 <div className={styles.productInfo}>
                                     <div className={styles.productName}>{products[1].name}</div>
+                                    <div className={styles.categoryName}>{products[1].category}</div>
                                     <div className={styles.productPrice}>{products[1].price}</div>
                                 </div>
                             </div>
@@ -139,6 +140,7 @@ const BestSellers = () => {
                                 <img alt={products[2].name} src={products[2].image} className={styles.productImage}/>
                                 <div className={styles.productInfo}>
                                     <div className={styles.productName}>{products[2].name}</div>
+                                    <div className={styles.categoryName}>{products[2].category}</div>
                                     <div className={styles.productPrice}>{products[2].price}</div>
                                 </div>
                             </div>
@@ -155,6 +157,7 @@ const BestSellers = () => {
                                  style={{width: '100%', height: 'calc(100% - 70px)'}}/>
                             <div className={styles.productInfo}>
                                 <div className={styles.productName}>{products[3].name}</div>
+                                <div className={styles.categoryName}>{products[3].category}</div>
                                 <div className={styles.productPrice}>{products[3].price}</div>
                             </div>
                         </div>
--- src/components/BestSellers/RotatingTextCircle.jsx
+++ src/components/BestSellers/RotatingTextCircle.jsx
@@ -2,28 +2,45 @@ import React from 'react';
 import styles from './RotatingTextCircle.module.scss';
 
 const RotatingTextCircle = () => {
+    const isMobile = window?.innerWidth < 768;
     const text = 'хиты продаж ';
-    const letters = text.repeat(4).split('');
+
+    const config = {
+        baseSize: isMobile ? 200 : 300,
+        fontSize: isMobile ? 18 : 20,
+        letterSpacing: isMobile ? 1.2 : 2.5,
+        dotSize: isMobile ? 8 : 12,
+        textRepeats: isMobile ? 3 : 4
+    };
+
+    const letters = text.repeat(config.textRepeats).split('');
 
     return (
-        <div className={styles.circleContainer}>
-            <div className={styles.circle}>
-                {letters.map((letter, index) => {
-                    const angle = (360 / letters.length) * index;
-                    return (
-                        <span
-                            key={index}
-                            className={styles.letter}
-                            style={{
-                                transform: `rotate(${angle}deg) translate(120px) rotate(${90}deg)`,
-                            }}
-                        >
-              {letter}
-            </span>
-                    );
-                })}
+        <div className={styles.circleWrapper} style={{ height: `${config.baseSize}px` }}>
+            <div className={styles.circleContainer}>
+                <div className={styles.circle}>
+                    {letters.map((letter, index) => {
+                        const angle = (360 / letters.length) * index;
+                        return (
+                            <span
+                                key={index}
+                                className={styles.letter}
+                                style={{
+                                    transform: `rotate(${angle}deg) translate(${config.baseSize/2 - 20}px) rotate(90deg)`,
+                                    fontSize: `${config.fontSize}px`,
+                                    letterSpacing: `${config.letterSpacing}px`
+                                }}
+                            >
+                                {letter}
+                            </span>
+                        );
+                    })}
+                </div>
+                <div className={styles.centerDot} style={{
+                    width: `${config.dotSize}px`,
+                    height: `${config.dotSize}px`
+                }}></div>
             </div>
-            <div className={styles.centerDot}></div>
         </div>
     );
 };
