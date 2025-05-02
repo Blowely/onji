e0d361b0c71fee3e90d27eb78a7d870f96849fe8
maryashin.andrey <maryashin.andrey@wb.ru>
2025-05-03 00:59:45 +0300

    implemented phone footer

---
+++ src/assets/svg/footer/categories-icon.jsx
@@ -0,0 +1,10 @@
+export const CategoriesIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="currentSize" height="currentSize" viewBox="0 0 52 46" fill="none">
+        <path
+            d="M32.5833 32.5833L40.25 40.25M5.75 21.0833C5.75 25.15 7.36547 29.0501 10.241 31.9256C13.1166 34.8012 17.0167 36.4167 21.0833 36.4167C25.15 36.4167 29.0501 34.8012 31.9256 31.9256C34.8012 29.0501 36.4167 25.15 36.4167 21.0833C36.4167 17.0167 34.8012 13.1166 31.9256 10.241C29.0501 7.36547 25.15 5.75 21.0833 5.75C17.0167 5.75 13.1166 7.36547 10.241 10.241C7.36547 13.1166 5.75 17.0167 5.75 21.0833Z"
+            stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+        <line x1="40" y1="10" x2="52" y2="10" stroke="currentColor" stroke-width="2"/>
+        <line x1="40" y1="28" x2="52" y2="28" stroke="currentColor" stroke-width="2"/>
+        <line x1="42" y1="19" x2="52" y2="19" stroke="currentColor" stroke-width="2"/>
+    </svg>
+);
--- src/components/PhoneFooter/PhoneFooter.jsx
+++ src/components/PhoneFooter/PhoneFooter.jsx
@@ -1,6 +1,12 @@
 import React, {useCallback, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import AuthModal from "../../pages/AuthModal";
+import styles from "./PhoneFooter.module.scss";
+import {HomeIcon} from "../../assets/svg/footer/home-icon";
+import {CategoriesIcon} from "../../assets/svg/footer/categories-icon";
+import {CartIcon} from "../../assets/svg/footer/cart-icon";
+import {FavIcon} from "../../assets/svg/footer/fav-icon";
+import {ProfileIcon} from "../../assets/svg/footer/profile-icon";
 
 const PhoneFooter = ({tab}) => {
     const navigate = useNavigate();
@@ -23,59 +29,61 @@ const PhoneFooter = ({tab}) => {
         {
             id: 1,
             tab: 'products',
+            Icon: HomeIcon,
             onClick: () => navigate(`/${gender}-products`),
-            src: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/1.%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2.png",
-            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/1.%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F.png",
+            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/test1/icons-onji/footer/home.svg",
             alt: "Главная",
-            style: { height: '26px' }
+            style: { height: '28px' }
         },
         {
             id: 2,
             tab: 'categories',
+            Icon: CategoriesIcon,
             onClick: () => navigate(`/${gender}-categories/`),
-            src: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/2.%D0%9A%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2.png",
-            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/2.%D0%9A%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3.png",
+            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/test1/icons-onji/footer/categories.svg",
             alt: "Каталог",
-            style: { height: '26px' }
+            style: { height: '28px' }
         },
         {
             id: 3,
             tab: 'cart',
+            Icon: CartIcon,
             onClick: () => navigate("/cart"),
-            src: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/3.%D0%9A%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D0%B0%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2.png",
-            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/3.%D0%9A%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D0%B0.png",
+            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/test1/icons-onji/footer/cart.svg",
             alt: "Корзина",
-            style: { height: '26px' }
+            style: { height: '28px' }
         },
         {
             id: 4,
             tab: 'favorites',
+            Icon: FavIcon,
             onClick: () => navigate("/favorites"),
-            src: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/4.%D0%98%D0%B7%D0%B1%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2.png",
-            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/4.%D0%98%D0%B7%D0%B1%D1%80%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5.png",
+            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/test1/icons-onji/footer/fav.svg",
             alt: "Избранное",
-            style: { height: '26px' }
+            style: { height: '28px' }
         },
         {
             id: 5,
             tab: 'profile',
+            Icon: ProfileIcon,
             onClick: onProfileClick,
-            src: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/5.%D0%9F%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8C%20%D0%B0%D0%BA%D1%82%D0%B8%D0%B2.png",
-            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/icons/v2/5.%D0%9F%D1%80%D0%BE%D1%84%D0%B8%D0%BB%D1%8C.png",
+            defaultSrc: "https://storage.yandexcloud.net/pc-mediafiles/test1/icons-onji/footer/profile.svg",
             alt: "Профиль",
-            style: { height: '26px' }
+            style: { height: '28px' }
         }
     ];
 
+
     const generateIcons = useCallback(() => {
-        return iconsData.map((el) => {
-            return (<div onClick={el.onClick} id={el.id} key={el.id}>
-                <img style={el.style}
-                     src={tab === el.tab ?  el.src : el.defaultSrc}
-                     alt={el.alt || ''}/>
-            </div>)
-        })
-    }, [tab])
+        return iconsData.map(({ id, tab: tabName, onClick, Icon, alt, style }) => {
+            const isActive = tab === tabName;
+            return (
+                <div className={isActive ? styles.active : ''} onClick={onClick} key={id} style={style}>
+                    <Icon />
+                </div>
+            );
+        });
+    }, [tab, gender]);
 
     return (<footer>
         {!token && openAuth &&
