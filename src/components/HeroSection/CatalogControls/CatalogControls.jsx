1aed8d07d3b1b040ae34a44da8e6a67c79364d45
maryashin.andrey <maryashin.andrey@wb.ru>
2025-05-06 02:20:14 +0300

    some styles changes

--- src/components/HeroSection/CatalogControls/CatalogControls.jsx
+++ src/components/HeroSection/CatalogControls/CatalogControls.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import filtersSvg from '../../../assets/svg/v2/filters.svg';
 import vectorDownSvg from '../../../assets/svg/v2/vector-down.svg';
 import catalogGreySvg from '../../../assets/svg/v2/catalog-grey.svg';
 import catalogSvg from '../../../assets/svg/v2/catalog.svg';
 import styles from './CatalogControls.module.scss';
+import {Select} from "antd";
+import {SORT_OPTIONS, SORT_TYPES} from "../../../pages/constants";
+import {useSearchParams} from "react-router-dom";
 
 const CatalogControls = ({setShowFilters}) => {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [offset, setOffset] = useState(1);
+    const [loading, setLoading] = useState(false);
+
+    const sortBy = searchParams.get("sortBy");
     const isDesktop = window?.innerWidth > 768;
+    const [sort, setSort] = useState(sortBy || 'by-relevance');
+    const handleChange = (value) => {
+        setLoading(true);
+        searchParams.set('sortBy', value);
+        setSearchParams(searchParams);
+        setSort(value);
+        setOffset(1);
+    };
 
     return (
         <div className={styles.catalogControlsWrapper}>
@@ -21,7 +38,7 @@ const CatalogControls = ({setShowFilters}) => {
                     />
                     фильтры
                 </span>
-                <span className={styles.catalogControlItem}>
+                {/*<span className={styles.catalogControlItem}>
                     по популярности
                     <img
                         src={vectorDownSvg}
@@ -30,7 +47,27 @@ const CatalogControls = ({setShowFilters}) => {
                         }}
                         alt=""
                     />
-                </span>
+                </span>*/}
+                <Select
+                    defaultValue={SORT_TYPES[sortBy] || SORT_TYPES["by-relevance"]}
+                    value={sort}
+                    size="middle"
+                    onChange={handleChange}
+                    options={SORT_OPTIONS}
+                    className={styles.selectCustom}
+                    suffixIcon={
+                        <img
+                            src={vectorDownSvg}
+                            style={{
+                                filter: 'brightness(0) saturate(100%)',
+                                width: '12px',
+                                marginLeft: '7px'
+                            }}
+                            alt=""
+                        />
+                    }
+                    popupClassName={styles.selectPopup}
+                />
             </div>
             {isDesktop &&
                 <div className={styles.catalogControlsBlock}>
