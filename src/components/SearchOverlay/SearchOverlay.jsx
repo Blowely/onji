dfd3d3780c9eb37fe494006d8c508d0871ed6868
maryashin.andrey <maryashin.andrey@wb.ru>
2025-05-18 15:24:56 +0300

    styles fix

--- src/components/SearchOverlay/SearchOverlay.jsx
+++ src/components/SearchOverlay/SearchOverlay.jsx
@@ -18,11 +18,25 @@ const SearchOverlay = ({ visible, onClose, setOverlayVisible, recentSearches, on
     const prevScrollY = useRef(0);
 
     useEffect(() => {
-        document.body.style.overflow = visible && 'hidden';
-        return () => {
+        if (visible) {
+            const scrollY = window.scrollY;
+            document.body.style.position = 'fixed';
+            document.body.style.top = `-${scrollY}px`;
+            document.body.style.left = '0';
+            document.body.style.right = '0';
+            document.body.style.overflow = 'hidden';
+            document.body.style.width = '100%';
+        } else {
+            const scrollY = document.body.style.top;
+            document.body.style.position = '';
+            document.body.style.top = '';
+            document.body.style.left = '';
+            document.body.style.right = '';
             document.body.style.overflow = '';
-        };
-    },[visible])
+            document.body.style.width = '';
+            window.scrollTo(0, parseInt(scrollY || '0') * -1);
+        }
+    }, [visible]);
 
     // при вводе в инпут
     const handleChange = async e => {
