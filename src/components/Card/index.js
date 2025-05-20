32a635b497297c39ce6d718c7dc5f97cbd71eae3
maryashin.andrey <maryashin.andrey@wb.ru>
2025-05-20 20:36:25 +0300

    fixed content loader

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -102,16 +102,17 @@ function Card({
       >
         {!name && (
             <ContentLoader
+                className={styles.contentLoader}
                 speed={0.8}
                 backgroundColor="#f3f3f3"
                 foregroundColor="#ecebeb"
-                className={styles.contentImgLoader}
+                width="100%"
+                height="100%"
+                viewBox="0 0 402 361"
+                preserveAspectRatio="none"
             >
-              <rect
-                  rx="10"
-                  ry="10"
-                  style={{ width: "100%", height: "100%" }}
-              />
+               теперь rect задаём в нужных координатах
+              <rect x="0" y="0" rx="10" ry="10" width="402" height="361" />
             </ContentLoader>
         )}
 
@@ -119,16 +120,18 @@ function Card({
             <>
               {loadingImg &&
                   <ContentLoader
+                      className={styles.contentImgLoader}
                       speed={0.8}
                       backgroundColor="#f3f3f3"
                       foregroundColor="#ecebeb"
-                      className={styles.contentImgLoader}
+                      // вот важное:
+                      width="100%"
+                      height="100%"
+                      viewBox="0 0 402 361"
+                      preserveAspectRatio="none"
                   >
-                    <rect
-                        rx="10"
-                        ry="10"
-                        style={{width: "100%", height: "100%"}}
-                    />
+                    {/* теперь rect задаём в нужных координатах */}
+                    <rect x="0" y="0" rx="10" ry="10" width="402" height="361" />
                   </ContentLoader>
               }
 
