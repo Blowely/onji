855eb461d4fee7e4ef9f071f85467e1e66cb153b
maryashin.andrey <maryashin.andrey@wb.ru>
2025-06-01 21:22:15 +0300

    motion.div duration: 0.3

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -486,7 +486,7 @@ function CategoryPage({ onAddToFavorite, onAddToCart }) {
                   id="productWrapper"
                   initial={{ x: '100%', opacity: 0 }}
                   animate={{ x: 0, opacity: 1 }}
-                  transition={{ type: 'tween', ease: 'easeInOut', duration: 0.2 }}
+                  transition={{ type: 'tween', ease: 'easeInOut', duration: 0.3 }}
                   style={{
                     position: 'fixed',
                     top: 0,
