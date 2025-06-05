72f753b53f8fee7f1587cc0973c939acb3cfe545
maryashin.andrey <maryashin.andrey@wb.ru>
2025-06-05 22:27:21 +0300

    isWebView && '-60px'

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -747,7 +747,7 @@ function CategoryPage({ onAddToFavorite, onAddToCart }) {
             className={`${styles.contentBlockHeader} ${search ? styles.searchActive : ''}`}
             style={{
               display: search ? 'flex' : 'grid',
-              paddingTop: isWebView && '60px'
+              marginTop: isWebView && '-60px'
             }}
             id="headerEl"
         >
